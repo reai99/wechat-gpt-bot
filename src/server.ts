d5c354e53d638e5f338baa614862cdba62383855
zhuqianyang <1367478101@qq.com>
2023-06-14 21:30:38 +0800

    feat:公众号开发

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,59 @@
-import Koa from 'koa';
-import route from 'koa-router';
+import Koa from "koa";
+import route from "koa-router";
+import cors from 'koa2-cors';
+import crypto from "crypto";
 import ChatGpt from "./lib/chatgpt.js";
-import Config from './config.js';
+import Config from "./config.js";
 
 const app = new Koa();
 const router = route();
 const chatGptClient: any = new ChatGpt();
 
-router.post('/api/common/reply', async (ctx) => {
+// 微信配置
+const webchatToken = "reaiWechat";
+
+// 跨域设置
+app.use(
+  cors({
+    origin: (ctx) => "*",
+    exposeHeaders: ["WWW-Authenticate", "Server-Authorization"],
+    maxAge: 5,
+    credentials: true,
+    allowMethods: ["GET", "POST", "DELETE"],
+    allowHeaders: ["Content-Type", "Authorization", "Accept"],
+  })
+);
+
+router.get("/", async (ctx) => {
+  const { signature, timestamp, nonce, echostr } = ctx.query;
+  let hash = crypto.createHash("sha1");
+  const arr = [webchatToken, timestamp, nonce].sort();
+  hash.update(arr.join(""));
+  const shasum = hash.digest("hex");
+  if (shasum === signature) {
+    return (ctx.body = echostr);
+  }
+  ctx.status = 401;
+  ctx.body = "Invalid signature";
+});
+
+// 消息回复
+router.post("/api/common/reply", async (ctx) => {
   const { ToUserName, FromUserName, Content, CreateTime } = ctx.request.body;
 
-  const question = (Content || '').trim();
+  const question = (Content || "").trim();
 
   try {
-    let reply = await chatGptClient.sendMessageToChatGpt(question, { id: ToUserName });
+    let reply = await chatGptClient.sendMessageToChatGpt(question, {
+      id: ToUserName,
+    });
     reply = Config.dealReply(reply, question);
 
     ctx.body = {
       ToUserName: FromUserName,
       FromUserName: ToUserName,
       CreateTime: +new Date(),
-      MsgType: 'text',
+      MsgType: "text",
       Content: reply,
     };
   } catch (error) {
@@ -28,12 +61,12 @@ router.post('/api/common/reply', async (ctx) => {
       ToUserName: FromUserName,
       FromUserName: ToUserName,
       CreateTime: +new Date(),
-      MsgType: 'text',
+      MsgType: "text",
       Content: question,
-    }
+    };
   }
 });
 
 app.use(router.routes());
-app.listen(8080);
-console.log('Server listen in:' + 8080);
+app.listen(80);
+console.log("Server listen in:" + 80);
